061285ad3a5b5f9d757bd71ac34f53e742c4eecf
AyushRay06 <rayayush47@gmail.com>
2024-11-19 17:46:06 +0530

    user-schema

--- backend/models/user.modal.js
+++ backend/models/user.modal.js
@@ -1,3 +1,34 @@
 import mongoose from "mongoose"
 
-//const userSchema = new mongoose.Schema({}, { timestamps: true })
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isVerified: {
+      type: Boolean,
+      dafault: false,
+    },
+    resetpasswordToken: String,
+    resetpasswordTokenExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.model("User", userSchema)
